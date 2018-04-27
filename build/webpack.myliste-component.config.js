1d8066d7b3068ca3ba62a0d531e7c970baed7d54
Maurice Pittet <maurice.pittet@lausanne.ch>
2018-04-27 15:06:32 +0200

    Settings for NPM publication

---
+++ build/webpack.myliste-component.config.js
@@ -0,0 +1,113 @@
+const webpack = require('webpack');
+const path = require('path');
+const utils = require('./utils')
+const APP_ABSOLUTE_PATH = path.join(__dirname, '../src');
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: path.join(APP_ABSOLUTE_PATH, '/components/MyListe.vue'),
+  output: {
+    library: 'mp-vue-myliste',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname + '/../dist/'),
+    filename: 'my-liste.component.js',
+    umdNamedDefine: true
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+            presets: ['babel-preset-env']
+        }
+        // include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(tiff|png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: require.resolve('jquery'),
+        use: [{
+            loader: 'expose-loader',
+            options: 'jQuery'
+        },{
+            loader: 'expose-loader',
+            options: '$'
+        }]
+      }
+    ]
+  },
+  externals: {
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin( {
+      minimize : true,
+      sourceMap : false,
+      mangle: true,
+      compress: {
+        warnings: false
+      }
+    } ),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"testing"',
+        UI_ENV: JSON.stringify(process.env.UI_ENV || '')
+      }
+    })
+  ]
+}
